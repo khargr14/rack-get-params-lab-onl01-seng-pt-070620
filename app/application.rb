7233e8c101db702aa58ff05f02dfe1b275e89af7
Keesha Hargrove <kharg14@gmail.com>
2020-09-22 22:19:28 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -30,4 +30,58 @@
 # end
 
 
+class Application
 
+  @@items = ["Apples","Carrots","Pears"]
+  @@cart = ["Apples", "Oranges"]
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+########### ITEMS PATH ###########
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+########### CART PATH ###########
+    elsif req.path.match(/cart/)
+      if @@cart.size == 0
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |c|
+          resp.write "#{c}\n"
+        end
+      end
+########### ADD PATH ###########
+    elsif req.path.match(/add/)
+      if @@items.include?(req.params.values.join(""))
+        @@items.each do |item|
+          @@cart << item
+          resp.write "added #{item}"
+        end
+      else
+        resp.write "We don't have that item"
+      end
+      
+########### NO PATH FOUND ###########
+    else
+      resp.write "Path Not Found"
+    end
+    resp.finish
+  end
+
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      if req.path.match(/add/)
+        return "We don't have that item"
+      end
+      return "Couldn't find #{search_term}"
+    end
+  end
+end
